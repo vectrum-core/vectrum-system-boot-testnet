d052c951219e90833f40579b110e2c31b897be6d
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2021-06-12 13:43:18 +0300

    update

--- 1_run_node.sh
+++ 1_run_node.sh
@@ -2,8 +2,11 @@
 
 mkdir -p ./data/node_bios
 
+#  --hard-replay-blockchain \
+#  --disable-replay-opts \
 vectrum-node \
   --data-dir=./data/node_bios \
   --config-dir=./config/node_bios \
   --genesis-json=./config/node_bios/genesis.json \
+  --disable-replay-opts \
   3>&1 1>> ./data/node_bios/debug.log 2>&1
